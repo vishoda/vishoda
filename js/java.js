08b51a47f3d554298106a2475c78273c9ca9ed8c
crazy_vishoda <164767177+vishoda@users.noreply.github.com>
2024-05-07 22:18:41 +0530

    Add files via upload

---
+++ js/java.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".kkkkk", {
+    strings : ["front end developer" , "YouTuber" , "web designing"],
+    typeSpeed : 100,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+})
